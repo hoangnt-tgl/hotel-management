a425a6d3a2e8ae6c5757aae79905c36a1bbbe425
Nguyen Trong Hoang <tronghoang1907@gmail.com>
2022-10-15 15:14:14 +0700

    1

--- routes/user.ts
+++ routes/user.ts
@@ -2,26 +2,27 @@ var express = require('express');
 var bcrypt = require('bcryptjs');
 var jwt = require("jsonwebtoken");
 var router = express.Router();
-
 var UserModel = require('../models/user');
 import { User } from '../models/user'
 
-router.post('/register', function (req, res, next) {
+const SECRET = process.env.SECRET;
+
+router.post('/register', function (req:any, res:any, next:any) {
     var email = req.body.email
     var lname = req.body.lname
     var fname = req.body.fname
     var dob = req.body.dob
     var phone = req.body.phone
-    var password = password
+    var password = req.body.password
 
-    UserModel.getUserByEmail(email, function (err, user) {
+    UserModel.getUserByEmail(email, function (err:any, user:any) {
         if (err) res.status(500).json(err);
         if (user) {
             res.json({ success: false, param: 'email', msg: 'Email already exists' });
         }
         else {
             var newUser = new User('', email, fname, lname, 0, '', dob, phone, 0, password)
-            User.createUser(newUser, function (err, user) {
+            newUser.createUser(newUser, function (err:any, user:any) {
                 if (err) res.status(500).json(err)
                 else {
                     res.json({ success: true, msg: 'Successful created new user' });
@@ -31,15 +32,15 @@ router.post('/register', function (req, res, next) {
     });
 });
 
-router.post('/login', function (req, res, next) {
-    UserModel.getUserByEmail(req.body.email, function (err, user) {
+router.post('/login', function (req:any, res:any, next:any) {
+    UserModel.getUserByEmail(req.body.email, function (err:any, users:Array<any>) {
         if (err) {
             res.status(500).json(err);
-        } else if (user == []) {
+        } else if (users.length === 0) {
             res.status(404).json('Incorrect email or password');
         } else {
-            user = user[0]
-            UserModel.comparePassword(req.body.password, user.user_password, function (err, isMatch) {
+            var user = users[0]
+            UserModel.comparePassword(req.body.password, user.user_password, function (err:any, isMatch:boolean) {
                 if (err) {
                     res.status(500).json(err);
                 } else if (isMatch) {
@@ -64,14 +65,8 @@ router.post('/login', function (req, res, next) {
 
 });
 
-// router.get('/me', function (req, res, next) {
-//     var user = req.user;
-//     user.password = undefined;
-//     res.json({ success: true, user: user });
-// });
-
-router.get('/getall', function (req, res, next) {
-    User.getAllUser(function (err, users) {
+router.get('/getall', function (req:any, res:any, next:any) {
+    UserModel.getAllUser(function (err:any, users:any){
         if (err) { res.status(500).json(err) }
         else {
             res.status(200).json(users)
@@ -79,51 +74,13 @@ router.get('/getall', function (req, res, next) {
     });
 });
 
-router.get('/get-by-username/:username', function (req, res, next) {
-    var username = req.params.username
-    User.getUserByUsername(username, function (err, users) {
-        if (err) { res.status(500).json(err) }
-        else {
-            res.status(200).json(users)
-        }
-    });
-});
 
-router.post('/delete', function (req, res, next) {
+router.post('/delete', function (req:any, res:any, next:any) {
     var selected = req.body.selected
-    User.deleteUser(selected, function (err, users) {
+    UserModel.deleteUser(selected, function (err:any, users:any) {
         if (err) { res.status(500).json(err) }
         else {
             res.status(200).json(users)
-        }
-    });
-});
-
-
-router.post('/activate', function (req, res, next) {
-    User.getUserByUsername(req.body.username, function (err, user) {
-        if (err) {
-            res.send(err);
-            return;
-        }
-        if (!user) {
-            res.json({ message: 'User not found' });
-            return;
-        }
-        if (user.status == 'block') {
-            res.json({ message: 'The account has been blocked' });
-            return;
-        }
-        if (user._id == req.body.id) {
-            User.updateUserStatus(user._id, 'active', function (err, user) {
-                if (err) {
-                    res.send(err);
-                    return;
-                }
-                res.json({ message: 'The account has been activated' });
-            });
-        } else {
-            res.json({ message: 'The activation link is invalid' });
         }
     });
 });
